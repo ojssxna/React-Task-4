0510134b07737f112a212ab5312798f928b45924
ojssxna <136606151+ojssxna@users.noreply.github.com>
2023-11-04 23:04:57 +0530

    Add files via upload

---
+++ React Task 4/React-Task4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Display from './DisplayData/DisplayData';
+
+function App() {
+  return (
+    <div className="App">
+      <Display/>
+    </div>
+  );
+}
+
+export default App;
